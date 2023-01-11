ecfd0a95afa963fd668714f2e042259d1d13231b
Haltimo Samuli <samuli.haltimo@metropolia.fi>
2023-01-11 15:10:34 +0200

    Laudan piirto - värit

--- shakki/kayttoliittyma.cpp
+++ shakki/kayttoliittyma.cpp
@@ -21,10 +21,34 @@ Kayttoliittyma* Kayttoliittyma::getInstance()
 
 void Kayttoliittyma::piirraLauta()
 {
+	// Ei tosiaan oo vihree
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_INTENSITY | BACKGROUND_RED |
 		BACKGROUND_GREEN | BACKGROUND_BLUE);
 	
+	// Tällä shakkinappulat toimii
 	_setmode(_fileno(stdout), _O_U16TEXT);
+
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			if (j == 0)
+			{
+				wcout << 8 - i << " ";
+			}
+			if (_asema->_lauta[i][j] == NULL)
+			{
+				wcout << "  ";
+			}
+			else
+			{
+				wcout << _asema->_lauta[i][j]->getUnicode() << " ";
+			}
+		}
+		wcout << "\n";
+	}
+
+	wcout << "\n  a b c d e f g h\n\n";
 }
 
 
