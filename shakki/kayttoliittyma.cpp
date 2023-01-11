7b45ff4b1c3bbe2ffc10878ba35f76bdad00f5f4
Haltimo Samuli <samuli.haltimo@metropolia.fi>
2023-01-11 15:26:24 +0200

    Toimiva shakkilaudan piirto

--- shakki/kayttoliittyma.cpp
+++ shakki/kayttoliittyma.cpp
@@ -22,9 +22,9 @@ Kayttoliittyma* Kayttoliittyma::getInstance()
 void Kayttoliittyma::piirraLauta()
 {
 	// Ei tosiaan oo vihree
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_INTENSITY | BACKGROUND_RED |
+	/*SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_INTENSITY | BACKGROUND_RED |
 		BACKGROUND_GREEN | BACKGROUND_BLUE);
-	
+	*/
 	// Tällä shakkinappulat toimii
 	_setmode(_fileno(stdout), _O_U16TEXT);
 
@@ -34,14 +34,35 @@ void Kayttoliittyma::piirraLauta()
 		{
 			if (j == 0)
 			{
+				// Laittaa numeron laudan vasempaan reunaan valkoisella värillä
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_INTENSITY | BACKGROUND_RED |
+					BACKGROUND_GREEN | BACKGROUND_BLUE);
 				wcout << 8 - i << " ";
 			}
+			
+			if ((i % 2 == 0 &&
+				(j % 2) - 1 == 0) ||
+				((i % 2) - 1 == 0 &&
+				 j % 2 == 0))
+			{
+				// Tuottaa parillisen rivin kuvioinnin laudalle vihreällä värillä
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_INTENSITY | BACKGROUND_GREEN);
+			}
+			else
+			{
+				// Resettaa värin valkoiseksi
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_INTENSITY | BACKGROUND_RED |
+					BACKGROUND_GREEN | BACKGROUND_BLUE);
+			}
+
 			if (_asema->_lauta[i][j] == NULL)
 			{
+				// Tulostaa välin nappuloiden väliin
 				wcout << "  ";
 			}
 			else
 			{
+				// Tulostaa nappulan unicoden
 				wcout << _asema->_lauta[i][j]->getUnicode() << " ";
 			}
 		}
